a3c02dc60c326e5389c79f057259176dce68e73a
Vishal Rajbhar <71400384+rvvishal@users.noreply.github.com>
2021-04-17 19:12:26 +0530

    Update and rename lineage_A33.mk to lineage_A33f.mk

--- lineage_A33f.mk
+++ lineage_A33f.mk
@@ -16,7 +16,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_k.mk)
-$(call inherit-product, device/oppo/A33/device.mk)
+$(call inherit-product, device/oppo/A33f/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Assert
@@ -26,11 +26,11 @@ TARGET_OTA_ASSERT_DEVICE := a33f,A33f,A33fw,a33fw,msm8916,msm8939
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
 TARGET_VENDOR := OPPO
-PRODUCT_DEVICE := A33
-PRODUCT_NAME := lineage_A33
+PRODUCT_DEVICE := A33f
+PRODUCT_NAME := lineage_A33f
 BOARD_VENDOR := OPPO
 PRODUCT_BRAND := OPPO
-PRODUCT_MODEL := A37
+PRODUCT_MODEL := A33f
 PRODUCT_MANUFACTURER := OPPO
 DEVICE_MAINTAINER := Hrx ( rvvishal )
 
